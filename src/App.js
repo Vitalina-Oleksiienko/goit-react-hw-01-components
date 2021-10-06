866bb85009abe9c7fa9fdf2e0563d71c3f4d0d89
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-10-06 10:22:07 +0300

    task1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import user from './components/Profile/profile.json';
+import user from './components/Profile/user.json';
 import Profile from './components/Profile/Profile';
 
-import statisticalData from './components/Statistics/statistics.json';
-import Statistics from './components/Statistics/Statistics';
+//import friends from './friends.json';
+//import FriendList from './components/Friends/FriendList';
 
-import friends from './components/FriendList/friend-list.json';
-import FriendList from './components/FriendList/FriendList';
+//import statisticalData from './statistical-data.json';
+//import StatList from './components/Statistics/Statistics';
 
-import transactions from './components/TransactionHistory/transactions-history.json';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+//import transaction from './transactions.json';
+//import TransactionHistory from './components/Transaction/Transaction';
+
 
 export default function App() {
   return (
-    <>
+    <div>
       <Profile
         name={user.name}
         tag={user.tag}
@@ -20,13 +21,7 @@ export default function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
-
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <Statistics stats={statisticalData} />
-
-      <FriendList friends={friends} />
-
-      <TransactionHistory items={transactions} />
-    </>
-  )
+      
+    </div>
+  );
 }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,46 +2,44 @@ import PropTypes from 'prop-types';
 import './profile.css';
 import defaultImg from './default-cat.png';
 
-
-export default function Profile ({
-    name,
-    tag,
-    location,
-    avatar = defaultImg,
-    stats,
+export default function Profile({
+  name,
+  tag,
+  location,
+  avatar = defaultImg,
+  stats,
 }) {
-    return (
-        <div className="profile">
-            <div className="description">
-                <img
-                    src={avatar}
-                    alt="Аватар пользователя"
-                    className="profile__avatar"
-                />
-                <p className="profile__name">{name}</p>
-                <p className="profile__tag">@{tag}</p>
-                <p className="profile__location">{location}</p>
-            </div>
+  return (
+    <div className="profile">
+      <div className="description">
+        <img
+          src={avatar}
+          alt="Аватар пользователя"
+          className="profile__avatar"
+          width="250"
+        />
+        <p className="profile__name">{name}</p>
+        <p className="profile__tag">@{tag}</p>
+        <p className="profile__location">{location}</p>
+      </div>
 
-            <ul className="profile__stats">
-                <li>
-                    <span className="label">Followers</span>
-                    <span className="quantity">{stats.followers}</span>
-                </li>
-                <li>
-                    <span className="label">Views</span>
-                    <span className="quantity">{stats.views}</span>
-                </li>
-                <li>
-                    <span className="label">Likes</span>
-                    <span className="quantity">{stats.likes}</span>
-                </li>
-            </ul>
-        </div>
-    );
-};
-
-
+      <ul className="profile__stats">
+        <li className="profile__stats-item">
+          <span className="label">Followers </span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li className="profile__stats-item">
+          <span className="label">Views </span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li className="profile__stats-item">
+          <span className="label">Likes </span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 Profile.propTypes = {
   name: PropTypes.string,
